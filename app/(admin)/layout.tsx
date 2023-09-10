09f3c2037c63a70693947410a2465c7a147996af
Axel Pettersson <axel@pettersson.cc>
2023-09-10 14:53:45 +0200

    Add loading when session ia authenticating

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,17 +1,18 @@
 'use client';
 
+import Loading from '@app/loading';
 import { signIn, useSession } from 'next-auth/react';
 import { PropsWithChildren } from 'react';
 
 export default function AdminLayout({ children }: PropsWithChildren) {
   const session = useSession();
 
-  if (session.status === 'unauthenticated') {
-    signIn();
+  if (session.status === 'loading') {
+    return <Loading />;
   }
 
-  if (session.status !== 'authenticated') {
-    return null;
+  if (session.status === 'unauthenticated') {
+    signIn();
   }
 
   return children;
